d85a4d93eb97c67a192829e24e9da7fa04f47b44
Malsly <cma120900@gmail.com>
2020-05-28 01:15:17 +0300

    Added stations

---
+++ TrainStation/ViewModels/StationViewModel.cs
@@ -0,0 +1,35 @@
+﻿using BL.Impl;
+using Entities;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ViewModels
+{
+    public class StationViewModel
+    {
+
+        public ObservableCollection<StationDTO> Stations
+        {
+            get;
+            set;
+
+        }
+        public StationDTO SelectedStation
+        {
+            get;
+            set;
+        }
+
+        public void LoadStations()
+        {
+            ObservableCollection<StationDTO> stations = new ObservableCollection<StationDTO>();
+
+            var stationService = new StationService();
+
+            foreach (StationDTO station in stationService.GetAll().Data.ToList<StationDTO>()) stations.Add(station);
+
+            Stations = stations;
+        }
+    }
+}
